250287c2e3140b5e2f61da3a4e4c52505577bad0
rkannappan <rkannappan@axioma.com>
2012-10-03 18:15:23 -0400

    Added method to get progress of task and also get it by using latest task instance start time.

--- DataControllerWebServices/src/main/java/com/axioma/datacontroller/webservice/TaskService.java
+++ DataControllerWebServices/src/main/java/com/axioma/datacontroller/webservice/TaskService.java
@@ -2,6 +2,14 @@ package com.axioma.datacontroller.webservice;
 
 import java.util.SortedSet;
 
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
 import javax.servlet.ServletContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -10,12 +18,15 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.commons.cli.ParseException;
+import org.joda.time.DateTime;
 
 import com.axioma.datacontroller.IDataController;
 import com.axioma.datacontroller.webservice.util.DatabaseConnector;
 import com.axioma.datacontroller.webservice.util.TaskConverter;
 import com.axioma.model.task.Task;
+import com.axioma.model.task.TaskInstance;
 import com.axioma.model.task.TaskType;
 import com.axioma.taskcommander.TaskCommander;
 import com.google.gson.Gson;
@@ -38,7 +49,7 @@ public class TaskService {
   }
   
   @GET
-  @Path("{run}/{taskType}/{taskName}")
+  @Path("/run/{taskType}/{taskName}")
   @Produces(MediaType.APPLICATION_JSON)
   public String runTaskByName(@PathParam("taskType") String taskType, @PathParam("taskName") String taskName) {
       String serversProps = context.getRealPath(DatabaseConnector.DB_PROPS_PATH);
@@ -46,5 +57,58 @@ public class TaskService {
       TaskCommander.runMain(new String[] { "-s", serversProps, "-t", taskType, "-r", taskName });
 	  
 	 return null;
+  }
+  
+  @GET
+  @Path("/progress/{taskType}/{taskName}")
+  @Produces(MediaType.APPLICATION_JSON)
+  public String getProgressByTaskName(@PathParam("taskType") String taskType, @PathParam("taskName") String taskName) {
+	  Connection connection = null;
+	  Session session = null;
+	  MessageConsumer consumer = null;
+	  try {
+		final ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(
+				ActiveMQConnectionFactory.DEFAULT_BROKER_URL);
+		connection = connectionFactory.createConnection();
+		connection.start();
+		session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		final DateTime startTime = this.getLatestTaskInstanceStartTime(taskType, taskName);
+		System.out.println("Queue is " + taskName + startTime.toString());
+		final Destination destination = session.createQueue(taskName + startTime.toString());
+		consumer = session.createConsumer(destination);
+
+			final Message m = consumer.receive(1000);
+			if (m != null) {
+				final TextMessage textMessage = (TextMessage) m;
+				String message = textMessage.getText();
+
+				Gson gson = new Gson();  
+				return gson.toJson(message);
+			}
+	  } catch (JMSException ex) {
+		  ex.printStackTrace();
+	  } finally {
+		  try {
+			  if (consumer != null) {
+				  consumer.close();
+			  }
+			  if (session != null) {
+				  session.close();
+			  }
+			  if (connection != null) {
+				  connection.close();
+			  }
+		  } catch (JMSException ex) {
+			  ex.printStackTrace();
+		  }
+	  }
+	  
+	  return null;
+  }
+  
+  private DateTime getLatestTaskInstanceStartTime(final String taskType, final String taskName) {
+	  IDataController dc = DatabaseConnector.getDataController(context);
+	  TaskInstance ti = dc.getLatest(new Task(taskName, TaskType.valueOf(taskType)));
+	  return ti.getStartDateTime();
   }
 }
